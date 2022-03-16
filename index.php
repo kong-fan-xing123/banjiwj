322b27951f1298cf6a92e3756a4640d683393c7f
kong-fan-xing123 <kfstary@outlook.com>
2022-03-16 19:16:25 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<?php
+$name = "孔繁星";
+?>
+<html>
+	<head>
+		<title><?php echo $name ?>的主页</title>
+	</head>
+	<body>
+		<a href="/home.php?name=<?php echo $name ?>">主页</a>
+
+	</body>
+</html>
